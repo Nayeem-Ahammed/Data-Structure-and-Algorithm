b99e3d39f13084c11ab7e7ee7814a34a7d91ff0c
Nayeem Ahammed <nayeem.ahammed01@gmail.com>
2026-01-26 21:46:44 +0600

    add pre required stl + basic recursion

---
+++ Graph/Pre_requerment.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    ## Recursion ##
+--------------------------
+return_type Func(Parameters) {
+    // 1. base case
+    if(base_condition) {
+        return base_value;
+    }
+
+    // 2. Recursive call on smaller input
+    return Func(smaller_input);
+}
+
+    ## vector ##
+--------------------------
+    v.push_back(x);   // O(1)
+    v.pop_back();     // O(1)
+    
+    ## stack ##
+---------------------------
+    st.push(x);      // O(1)
+    st.top();        // O(1)
+    st.pop();        // O(1)
+
+    ## queue ##
+--------------------------
+    q.push(x);      // O(1)
+    q.front();      // O(1)
+    q.back();       // O(1)
+    q.pop();        // O(1)
+
+    ## priority_queue ##
+-------------------------------
+    priority_queue<int> pq; // Max Heap [parent >= child]
+    priority_queue<int, vector<int>, greater<int>> pq; // Min Heap [parent <= child]
+
+    pq.push(x);     // O(log n)
+    pq.pop();       // O(log n)
+    pq.top();       // O(1)
+
+*/
+int main() {
+
+}
